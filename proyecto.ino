ca8f1875d8c391e38007a299534f3d9b66887398
Daniela Mejia Rivas <81272157+danielaMR28@users.noreply.github.com>
2025-04-23 12:59:41 -0600

    Create proyecto.ino

---
+++ proyecto.ino
@@ -0,0 +1,143 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+#define IR_SENSOR_PIN 2 
+
+// Replace with your network credentials
+const char* ssid = "nombre_internet";
+const char* password = "contrasena";
+
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Create a WebSocket object
+AsyncWebSocket ws("/ws");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1000;
+
+
+// Get Sensor Readings and return JSON object
+String getSensorReadings(){
+
+
+
+  int irValue1 = digitalRead(4);  // Replace with your actual pin
+  int irValue2 = digitalRead(16);
+
+  String status1 = (irValue1 == LOW) ? "0" : "1";
+  String status2 = (irValue2 == LOW) ? "0" : "1";
+  if (digitalRead(4) == 1){
+
+      digitalWrite(23, HIGH);
+
+  }
+  else {
+      digitalWrite(23, LOW);
+  }
+
+  String jsonString = "{\"ir_sensor1\":\"" + status1 + "\",\"ir_sensor2\":\"" + status2 + "\"}";
+
+  return jsonString;
+}
+
+// Initialize LittleFS
+void initLittleFS() {
+  if (!LittleFS.begin(true)) {
+    Serial.println("An error has occurred while mounting LittleFS");
+  }
+  Serial.println("LittleFS mounted successfully");
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void notifyClients(String sensorReadings) {
+  ws.textAll(sensorReadings);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    //data[len] = 0;
+    //String message = (char*)data;
+    // Check if the message is "getReadings"
+    //if (strcmp((char*)data, "getReadings") == 0) {
+      //if it is, send current sensor readings
+      String sensorReadings = getSensorReadings();
+      Serial.print(sensorReadings);
+      notifyClients(sensorReadings);
+    //}
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void setup() {
+  Serial.begin(115200);
+  initWiFi();
+  initLittleFS();
+  initWebSocket();
+
+  pinMode(4, INPUT);
+  pinMode(23, OUTPUT);
+  pinMode(16, INPUT);
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    String sensorReadings = getSensorReadings();
+    Serial.println(sensorReadings);
+    notifyClients(sensorReadings);
+    lastTime = millis();
+  }
+  ws.cleanupClients();
+  
+}
